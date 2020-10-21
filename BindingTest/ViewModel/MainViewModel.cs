73dae84c600600277758296795955c4191b20e79
범희 조 <afsdzvcx123@naver.com>
2020-10-21 19:03:17 +0900

    초기 커밋.

---
+++ BindingTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BindingTest
+{
+    public class MainViewModel
+    {
+        private ICommand connectCommand;
+        public ICommand ConnectCommand
+        {
+            get
+            {
+                return (this.connectCommand) ?? 
+                    (this.connectCommand = new DelegateCommand(Connect));
+            }
+        }
+
+        private ICommand cancelCommand;
+        public ICommand CancelCommand
+        {
+            get
+            {
+                return (this.cancelCommand) ?? 
+                    (this.cancelCommand = new DelegateCommand(Cancel));
+            }
+        }
+
+        private ICommand updateCommand;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                return (this.updateCommand) ?? (this.updateCommand = new DelegateCommand(Update));
+            }
+        }
+
+        private void Update()
+        {
+            MessageBox.Show($"Update");
+        }
+
+        private void Connect()
+        {
+            MessageBox.Show($"Connect");
+        }
+
+        private void Cancel()
+        {
+            MessageBox.Show($"Cancel");
+        }
+    }
+}
